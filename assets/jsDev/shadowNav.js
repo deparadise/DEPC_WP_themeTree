4865beba69e07c703c6dba91fc684f849e534d22
David Pineda <david.pineda@deparadise.com>
2016-11-17 00:24:53 -0800

    hover in and out produces the desired scroll-fade effect

--- assets/jsDev/shadowNav.js
+++ assets/jsDev/shadowNav.js
@@ -15,21 +15,45 @@ $(document).ready(function() {
 		setMainNavBehaviorOn: function(trigger, seriesCB) {
 			//console.log('test trigger:', trigger);
 			var targetMenu = $(trigger).children('.sub-menu');
-			// $(targetMenu).css({
-			// 	'display' : 'none'//,
-			// 	//'opacity' : '0'//,
-			// 	//height
-			// 	//'border' : '1px solid green'
-			// });
+			var targetMenuHeight = $(targetMenu).height();
 
 			$(trigger).hover(
+				// mouse in
 				function() {
-					//$(this).css('border','1px solid blue');
-					$(targetMenu).css('display', 'none').fadeIn();
+					// $(targetMenu).css('display', 'none').fadeIn();
+					$(targetMenu)
+					.stop(true)
+					.css({
+						'display': 'block',
+						'height': '0px',
+						'opacity': '0',
+						// 'border': '1px solid green'
+					})
+					.animate({
+						'height': targetMenuHeight,
+						'opacity':'1'
+					}, 300);
 				},
+				// mouse out
 				function() {
-					//$(this).css('border','1px solid fuchsia');
-					$(targetMenu).css('display', 'block').fadeOut();
+					//$(targetMenu).css('display', 'block').fadeOut();
+					$(targetMenu)
+					.stop(true)
+					.css({
+						'display': 'block',
+						'height': targetMenuHeight,
+						'opacity': '1',
+						// 'border': '1px solid green'
+					})
+					.stop(true)
+					.animate({
+						'height': '0px',
+						'opacity':'0'
+					}, 300, function() {
+						$(targetMenu).css({
+							'display': 'none'
+						});
+					});
 				}
 			);
 
