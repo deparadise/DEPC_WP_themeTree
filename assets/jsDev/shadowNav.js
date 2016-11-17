ee53c8c030686e3299ea1dec59435ea9e7288bfe
David Pineda <david.pineda@deparadise.com>
2016-11-16 22:46:48 -0800

    adding on document,ready() and async control flow to the shadowNav module

--- assets/jsDev/shadowNav.js
+++ assets/jsDev/shadowNav.js
@@ -1,22 +1,46 @@
 //console.log('shadowNav.js loaded...');
+$(document).ready(function() {
 
+	var shadowNav = {
+		navTriggers: [],
+		targetNavComponents: function(cb) {		
+			this.navTriggers = jQuery(
+				'.main-nav > .menu-item-has-children'
+			);//.filter('.main-nav > li');
 
-var shadowNav = {
-	navTriggers: [],
-	targetNavComponents: function(cb) {		
-		this.navTriggers = jQuery(
-			'.main-nav > .menu-item-has-children'
-		);//.filter('.main-nav > li');
+			if (cb) {
+				return cb();
+			}
+		},
 
 		if (cb) {
 			return cb();
 		}
 	}
-}
 
 
-setTimeout(function() {
-	shadowNav.targetNavComponents(function(){
-		console.log('navTriggers', shadowNav.navTriggers);
-	});
-}, 1000);
+	async.waterfall(
+		[
+			// target triggers
+			function(cb) {
+				shadowNav.targetNavComponents(function(){
+					//console.log('navTriggers', shadowNav.navTriggers);
+					return cb(null);
+				});
+			},
+			// Behavior
+			function(cb) {
+				shadowNav.assignBehavior(cb);
+			}
+		],
+		function(err) {
+			if (err) {
+				console.error(err);
+			}else{
+				console.log('> the Shadow Nav is setup!');
+			}
+		}
+	);
+
+///
+}); // END shadowNav.js
