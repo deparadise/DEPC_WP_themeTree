ce961e5503b40233628c2c72b155e0aaa3c5a6ac
David Pineda <david.pineda@deparadise.com>
2016-11-17 11:16:56 -0800

    versioned 0.0.1_shadowNav on screen

--- assets/js/allThemeControl.js
+++ assets/js/allThemeControl.js
@@ -2,28 +2,117 @@
 $(document).foundation();
 //console.log('mainNav.js loaded!');
 //console.log('shadowNav.js loaded...');
+$(document).ready(function() {
 
+	var shadowNav = {
+		navTriggers: [],
+		targetNavComponents: function(cb) {		
+			this.navTriggers = jQuery(
+				'.main-nav > .menu-item-has-children'
+			);//.filter('.main-nav > li');
 
-var shadowNav = {
-	navTriggers: [],
-	targetNavComponents: function(cb) {		
-		this.navTriggers = jQuery(
-			'.main-nav > .menu-item-has-children'
-		);//.filter('.main-nav > li');
+			if (cb) {
+				return cb();
+			}
+		},
+		setMainNavBehaviorOn: function(trigger, seriesCB) {
+			//console.log('test trigger:', trigger);
+			var targetMenu = $(trigger).children('.sub-menu');
+			var targetMenuHeight = $(targetMenu).height();
 
-		if (cb) {
-			return cb();
+			$(trigger).hover(
+				// mouse in
+				function() {
+					// $(targetMenu).css('display', 'none').fadeIn();
+					$(targetMenu)
+					.stop(true)
+					.css({
+						'display': 'block',
+						'height': '0px',
+						'opacity': '0',
+						// 'border': '1px solid green'
+					})
+					.animate({
+						'height': targetMenuHeight,
+						'opacity':'1'
+					}, 300);
+				},
+				// mouse out
+				function() {
+					//$(targetMenu).css('display', 'block').fadeOut();
+					$(targetMenu)
+					.stop(true)
+					.css({
+						'display': 'block',
+						'height': targetMenuHeight,
+						'opacity': '1',
+						// 'border': '1px solid green'
+					})
+					.stop(true)
+					.animate({
+						'height': '0px',
+						'opacity':'0'
+					}, 300, function() {
+						$(targetMenu).css({
+							'display': 'none'
+						});
+					});
+				}
+			);
+
+			return seriesCB(null);
+		},
+		assignBehavior: function(cb) {
+			var shadowNav = this;
+
+			async.eachOfSeries(
+				//
+				shadowNav.navTriggers,
+				//
+				function(trigger, index, seriesCB) {
+					shadowNav.setMainNavBehaviorOn(trigger, seriesCB)
+
+					// return seriesCB(null);
+				},
+				//
+				function(err) {
+					if (err) {
+						//err
+					}else{
+						return cb(null);
+					}
+				}
+			);
 		}
 	}
-}
 
 
-setTimeout(function() {
-	shadowNav.targetNavComponents(function(){
-		console.log('navTriggers', shadowNav.navTriggers);
-	});
-}, 1000);
-console.log('simple slider controller loaded!');
+	async.waterfall(
+		[
+			// target triggers
+			function(cb) {
+				shadowNav.targetNavComponents(function(){
+					//console.log('navTriggers', shadowNav.navTriggers);
+					return cb(null);
+				});
+			},
+			// Behavior
+			function(cb) {
+				shadowNav.assignBehavior(cb);
+			}
+		],
+		function(err) {
+			if (err) {
+				console.error(err);
+			}else{
+				console.log('> the Shadow Nav is setup!');
+			}
+		}
+	);
+
+///
+}); // END shadowNav.js
+// console.log('simple slider controller loaded!');
 
 $(document).ready(function(){
 	$('.simple-slider').slick({
