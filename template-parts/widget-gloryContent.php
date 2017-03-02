d14a43009893bf2a1bbb9fc242713d9b4c652a26
David Pineda <david.pineda@deparadise.com>
2017-03-01 22:34:45 -0800

    Assorted style changes to get the theme going

--- template-parts/widget-gloryContent.php
+++ template-parts/widget-gloryContent.php
@@ -1,4 +1,4 @@
-<div class="glory-display widget">
+<div class="glory-display widget columns">
 	<?php
 		if ( function_exists('ACF') ) {
 			if (get_field('glory_display') && is_page_template('home.php')) {
