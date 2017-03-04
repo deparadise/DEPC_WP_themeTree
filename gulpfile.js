0080c9b996168fdb689d33f329b70b69241d277f
David Pineda <david.pineda@deparadise.com>
2017-03-04 10:36:40 -0800

    Gulpfile catch up

--- gulpfile.js
+++ gulpfile.js
@@ -1,101 +1,195 @@
-/// DEPENDENCIES
-	var 	gulp = require('gulp'),
-			compass = require('gulp-compass'),
-			concat = require('gulp-concat'),
-			gulpIf = require('gulp-if'),
-			jsHint = require('gulp-jshint'),
-			livereload = require('gulp-livereload'),
-			uglify = require('gulp-uglify');
-			gutil = require('gulp-util');
+/// GULP WORKFLOWS
+'use strict';
 
-/// WORKFLOW
+/// DEPENDENCIES
+	var gulp		= require('gulp'),
+		compass		= require('gulp-compass'),
+		jsHint 		= require('gulp-jshint'),
+		cache		= require('gulp-cached'),
+		stylish		= require('jshint-stylish'),
+		clean		= require('gulp-clean'),
+		runSequence = require('run-sequence'),
+
+		browserSync = require('browser-sync'),				//https://browsersync.io/docs/gulp
+		//livereload = require('gulp-livereload'),
+
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify'),
+		gulpIf = require('gulp-if'),
+
+		gUtil = require('gulp-util');
+
+// SETTINGS
+	// Workflow selection
+	var selectedReloadDelay = (true)? 6000 : 0;
+	var production = false;
+
 	// Workflow parameters
-	var 	production,
+	var 	styleComments,
 			sassStyle,
 			uglifyJS;
-	
-	// Workflow selection
-	production = false;
-	
-	if (production === true) {
-		// Production parameters
-		 sassStyle = 'compressed';
-		 uglifyJS = true;
-		 gutil.log('Process for production!');
-	}else{
-		// Development parameters
-		 sassStyle = 'expanded';
-		 uglifyJS = false;
-		 gutil.log('Process for development!');
-	}
-	
 
-/// ASSET SOURCE LIST
+
+// MODE CONFIGURATION
+	gulp.task('config_mode', function(){
+		if (production) {
+			gUtil.log('Production config!');
+			// Production parameters
+			styleComments = false;
+			sassStyle = 'compressed';
+			uglifyJS = true;
+		}else{
+			gUtil.log('Development config!');
+			// Development parameters
+			styleComments = true;
+			sassStyle = 'expanded';
+			uglifyJS = false;
+		}
+
+		console.log(
+			'\nproduction:', production,
+			'\nstyleComments:', styleComments,
+			'\nsassStyle:', sassStyle,
+			'\nuglifyJS:', uglifyJS
+		);
+	});
+
+
+// ASSETS
+	var targetMainSass 	=	'assets/sass/style.scss';
+	var sassSources 	=	['assets/sass/**/*.scss'];
+
 	// Designate dev js files
-	var jsSources = ['assets/jsDev/*.js']; 
+	var scriptSources = ['assets/jsDev/*.js'];
 	//  Designate dev php files
-	var staticSources = ['./**/*.php'];
-	// Designate dev sass files
-	var sassSources = ['assets/sass/**/*.scss'];
-	
-	
-/// GULP PIPE FUNCTIONS
-	// static >>> all php static files pipe
-	gulp.task('static', function(){
-		gulp.src(staticSources)
-		.pipe(livereload())
+	var staticTemplates = ['./**/*.php'];
+
+// TARGET COMPOENTS
+	var buildComponents = [
+		'allScript.js',
+		'style.css'
+	];
+
+// CLEAN COMPONENTS
+	gulp.task('clean_up', function(){
+		// work around for compass overwrite fail
+		console.log('Removing:', buildComponents[1]);
+		gulp.src(buildComponents[1], {read: false}).pipe(clean());
 	});
 
-	// compass >>> Styles pipe
-	gulp.task('compass', function(){
-		gulp.src('assets/sass/style.scss')
-		.pipe(compass({
-			// COMPASS CONFIGURATION
-			sass: 'assets/sass',
-			css: '.',
-			comments: true,
-			style: sassStyle,  // expanded for dev / compressed for prod
-			image: 'assets/imgs'
-		})
-		.on('error', gutil.log))
-		.pipe(gulp.dest('.'))
-		.pipe(livereload())
+// COMPASS / SASS
+	gulp.task('do_compass', function(){
+		gulp.src(targetMainSass)
+		.pipe(
+			// Configure my compass...
+			compass({
+				sass: 'assets/sass',
+				css: '.',
+				comments: styleComments,
+				style: sassStyle, 							// expanded for dev / compressed for prod
+				image: 'assets/imgs'
+			})
+		)
+		.on('error', gUtil.log);
+		//.pipe(gulp.dest('.tmp/styles/styles.css'))		// Alternate desitnations if needed
 	});
 
-	// js >>> Script files pipe
-	gulp.task('js', function(){
-		gulp.src(jsSources)
-		
+
+// CHECK JS
+	gulp.task('check_js', function(){
+		return gulp.src(scriptSources)
+		.pipe(cache('linting'))
 		.pipe(jsHint())
-		.pipe(jsHint.reporter('default'))
-		
-		.pipe(concat('allScript.js'))
-		.pipe(gulpIf(uglifyJS === true, uglify()))
-		.pipe(gulp.dest('assets/js'))
-		.pipe(livereload())
+		.pipe(jsHint.reporter(stylish));
 	});
 
 
-/// GULP RUNNING FUNCTIONS
-// watch >>> Watches files for changes and runs workflow pipes
-	gulp.task('watch', function(){
-		livereload.listen();
-		gulp.watch(staticSources, ['static']);
-		gulp.watch(sassSources, ['compass']);
-		gulp.watch(jsSources, ['js']);
+// COMBINE JS
+	gulp.task('combine_js', ['check_js'], function(){
+		gulp.src(scriptSources)
+		.pipe(concat(buildComponents[0]))
+		.pipe(gulpIf(uglifyJS, uglify()))
+		.pipe(gulp.dest('.'));
 	});
 
 
-/// DEFAULT >>> Run all dev tasks and init 'watch'
-	gulp.task('default', [
-		//'static', 
-		'compass', 
-		'js', 
-		'watch'
-	]);
-	
-	
-/// log >>> Test logging
-	gulp.task('log', function(){
-		gutil.log('Workflows are awesome!');
+// WATCH
+	gulp.task('watch_these', function() {
+		// styles
+		gulp.watch(sassSources, ['do_compass'])
+			.on('change', function(){
+				console.log('> waiting for compass to compile the sass...');
+				setTimeout(function() {
+					console.log('... OK GO!');
+					browserSync.reload();
+				}, selectedReloadDelay);
+			});
+		// Templates
+		gulp.watch(staticTemplates)
+			.on('change', browserSync.reload);
+		// Scripts
+		gulp.watch(scriptSources, ['combine_js'])
+			.on('change', browserSync.reload);
 	});
+
+
+// SERVER
+	gulp.task('serve', function(){
+		// Browser Sync otions: https://browsersync.io/docs/options
+		browserSync.init({
+			//port: 3000,
+			//startPath: 'http://themebench03.dev/', 			// specific starting point
+			open: true,
+			proxy: {
+				target: 'uipromos.themebench.dev',
+				ws: true
+			}//,
+			// server: {} 			// MAMP takes care of this...
+		});
+	});
+
+
+// GULP COMMANDS
+gulp.task('default', [
+	'config_mode',
+	'do_compass',
+	'combine_js',
+	'serve',
+	'watch_these'
+]);
+
+gulp.task('distribute_dev', function(){
+	production = false;
+
+	runSequence(
+	'clean_up',
+	'config_mode',
+	'do_compass',
+	'combine_js',
+	function (error) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(' DEV RELEASE FINISHED SUCCESSFULLY');
+		}
+	});
+
+});
+
+gulp.task('distribute_production', function(){
+	production = true;
+
+	runSequence(
+	'clean_up',
+	'config_mode',
+	'do_compass',
+	'combine_js',
+	function (error) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(' PRODUCTION RELEASE FINISHED SUCCESSFULLY! :0)');
+		}
+	});
+
+});
