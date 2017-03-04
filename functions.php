f5e43ba73493c14d1d5c15693e45db87d9c80876
David Pineda <david.pineda@deparadise.com>
2017-03-04 11:21:20 -0800

    Following new js production logic

--- functions.php
+++ functions.php
@@ -155,7 +155,7 @@ function depc_scripts() {
 	wp_enqueue_style( 'depc-style', get_stylesheet_uri() );
 	
 	// Collected custom scripts
-	wp_enqueue_script( 'depc-all-script', get_template_directory_uri() . '/assets/js/allScript.js', array(), '01', true );
+	wp_enqueue_script( 'depc-all-script', get_template_directory_uri() . '/allScript.js', array(), '01', true );
 
 	//wp_enqueue_script( 'depc-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
