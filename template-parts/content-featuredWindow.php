3ca0ccd7c7b6882bc0fb7b4d3e8c7352c06e5ff0
David Pineda <david.pineda@deparadise.com>
2017-03-05 20:23:42 -0800

    static featured-window

--- template-parts/content-featuredWindow.php
+++ template-parts/content-featuredWindow.php
@@ -5,7 +5,7 @@
 ">
 	<?php // DEPC todo: account for archive template
 	if(is_front_page()) {
-	
+
 		if ( function_exists('ACF') ) {
 			if (get_field('simple_slider') && is_page_template('home.php')) {
 				$url_collection = get_field('simple_slider');
@@ -46,7 +46,7 @@
 		}
 
 	}elseif ( is_archive() ) {
-		
+
 		//the_archive_title( '<h1 class="page-title">', '</h1>' );
 
 		$archiveTitle = get_the_archive_title();
@@ -55,34 +55,34 @@
 
 		echo '<h1 class="page-title">' . $archiveTitle . '</h1>';
 
-	}else{ // DEFAULT HEADER DISPLAYS	
+	}else{ // DEFAULT HEADER DISPLAYS
 
 			if($pageTemplate == 'Archive-Posts') :?>
 			<?php //echo("<span style=\"color: red;\">ARCHIVE POSTS!</span>");
 				$thumbCheck = z_taxonomy_image_url();
-				//echo z_taxonomy_image_url(); 
+				//echo z_taxonomy_image_url();
 			?>
 			<?php if ($thumbCheck == !null):
-				
+
 			?>
 				<img src="<?php if (function_exists('z_taxonomy_image_url')) echo z_taxonomy_image_url(); ?>"/>
 			<?php endif;?>
-		
+
 		<?php // DEPC todo: account for Single Post
 
 			elseif ($pageTemplate == 'Single-Post') : ?>
 			<?php //echo("<span style=\"color: green;\">SINGLE ARCHIVE POST!</span>");?>
-			 <img src="<?php echo z_taxonomy_image_url($currentTerm); ?>" />				
-			 
+			 <img src="<?php echo z_taxonomy_image_url($currentTerm); ?>" />
+
 		<?php else:// DEFAULT PAGES?>
-			
+
 			<?php echo get_the_post_thumbnail($page->ID, 'full'); ?>
-		
+
 		<?php endif;?>
 
 		<span class="img-screen"></span>
 
-		<header class="entry-header">
+		<header class="entry-header columns medium-10 medium-push-1">
 			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</header><!-- .entry-header -->
 	<?php } ?>
